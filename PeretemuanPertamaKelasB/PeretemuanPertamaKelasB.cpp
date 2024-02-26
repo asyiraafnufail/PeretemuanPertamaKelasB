a842d73cebebca1be1bb10cdd05911470934d746
asyiraafnufail <asyiraafnufail@gmail.com>
2024-02-26 15:25:15 +0700

    mengembalikan variabel yang dihapus

--- PeretemuanPertamaKelasB/PeretemuanPertamaKelasB.cpp
+++ PeretemuanPertamaKelasB/PeretemuanPertamaKelasB.cpp
@@ -14,4 +14,12 @@ int main()
     //compute nLuas= nPanjang * nLebar
     // display 'Luasnya adalah = ' + nLuas
 
+    int nPanjang, nLebar, nLuas;
+    cout << "Masukan Panjang = ";
+    cin >> nPanjang;
+    cout << "Masukan Lebar = ";
+    cin >> nLebar;
+    nLuas = nPanjang * nLebar;
+    cout << "Luasnya adalah " << nLuas << endl;
+
 }//end
