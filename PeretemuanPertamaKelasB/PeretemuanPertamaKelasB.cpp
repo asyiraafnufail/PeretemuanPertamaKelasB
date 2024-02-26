c804be02f16ed141fd28e9b050854f4c66781257
asyiraafnufail <asyiraafnufail@gmail.com>
2024-02-26 15:24:31 +0700

    Menghapus sebagian variabel

--- PeretemuanPertamaKelasB/PeretemuanPertamaKelasB.cpp
+++ PeretemuanPertamaKelasB/PeretemuanPertamaKelasB.cpp
@@ -14,12 +14,4 @@ int main()
     //compute nLuas= nPanjang * nLebar
     // display 'Luasnya adalah = ' + nLuas
 
-    int nPanjang, nLebar, nLuas;
-    cout << "Masukan Panjang = ";
-    cin >> nPanjang;
-    cout << "Masukan Lebar = ";
-    cin >> nLebar;
-    nLuas = nPanjang * nLebar;
-    cout << "Luasnya adalah " << nLuas << endl;
-
 }//end
